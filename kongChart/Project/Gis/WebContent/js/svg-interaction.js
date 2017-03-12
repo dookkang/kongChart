774cf2dc08f024f91957d9b1a99dc16ec02eb505
kim <kim@kim-PC>
2017-03-12 15:09:01 +0900

    삭제

--- kongChart/Project/Gis/WebContent/js/svg-interaction.js
+++
@@ -1,8 +0,0 @@
-var moveSlider = function(slider, direction) {
-	var value = slider.value;
-	var svg = document.getElementById("circle-svg"); 
-	var svgDoc = svg.contentDocument;
-	var circle = svgDoc.getElementById("my-circle");
-	//console.log('>'+circle);
-	circle.setAttributeNS(null, "c" + direction, value * 5);
-}
