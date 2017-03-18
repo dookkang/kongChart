f9f3d7c783afac7c06dd272e5c95dbf4a8129d76
kim <kim@kim-PC>
2017-03-18 23:36:03 +0900

    게시판소스첫올림

--- kongChart/php/etc/meta.php
+++
@@ -1,5 +0,0 @@
-<meta http-equiv="Content-type" content="text/html; charset=UTF-8">	
-<meta name="Author" content="DuHan.kim">
-<meta name="Keywords" content="kong chart">
-<meta name="Description" content="kong chart">
-<link rel="stylesheet" type="text/css" href="/css/main.css" />
